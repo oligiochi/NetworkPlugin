bf2da651893fa316cd10bff8084a0981036afab6
Mariano Scazzariello <marianoscazzariello@gmail.com>
2025-02-27 16:59:13 +0100

    Fix vde compilation + Set tap speed to 10G using ioctl

--- vde/go-src/src/lib/tap_utils.go
+++ vde/go-src/src/lib/tap_utils.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"net"
 	"strings"
+	"syscall"
+	"unsafe"
 
 	"github.com/google/uuid"
 	"github.com/vishvananda/netlink"
 	"github.com/containernetworking/plugins/pkg/ns"
+	"golang.org/x/sys/unix"
 )
 
 const (
@@ -15,10 +18,60 @@ const (
 	tapLen		= 8
 )
 
+/* Struct that holds the ethtool link settings, converted from C */
+type ethtoolCmd struct {
+	Cmd           uint32
+	Supported     uint32
+	Advertising   uint32
+	Speed         uint16
+	Duplex        uint8
+	Port          uint8
+	PhyAddress    uint8
+	Transceiver   uint8
+	Autoneg       uint8
+	MdioSupport   uint8
+	Maxtxpkt      uint32
+	Maxrxpkt      uint32
+	SpeedHi       uint16
+	EthTpMdix     uint8
+	EthTpMdixCtrl uint8
+	LpAdvertising uint32
+	Reserved      [2]uint32
+}
+
 func randomTapName() string {
 	randomUuid, _ := uuid.NewRandom()
 
 	return tapPrefix + strings.Replace(randomUuid.String(), "-", "", -1)[:tapLen]
+}
+
+/* Set the tap interface speed through ioctl, using the ethtool endpoints */
+func SetTapSpeed(tapIface string) (error) {
+	fd, err := syscall.Socket(unix.AF_INET, unix.SOCK_DGRAM, 0)
+	if err != nil {
+		return err
+	}
+	defer syscall.Close(fd)
+
+	ecmd := &ethtoolCmd {
+		Cmd: 0x00000002, 	/* ETHTOOL_SSET - Set ethtool_link_settings */	
+		Duplex: 0x01,		/* FULL_DUPLEX */
+	}
+
+	/* 10G - Expressed as 1Mb */
+	fullSpeed := uint32(10000)
+	ecmd.Speed = uint16(fullSpeed & 0xffff)
+	ecmd.SpeedHi = uint16(fullSpeed >> 16)
+
+	ifreq := &netlink.Ifreq{Data: uintptr(unsafe.Pointer(ecmd))}
+	copy(ifreq.Name[:unix.IFNAMSIZ-1], tapIface)
+
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd), netlink.SIOCETHTOOL, uintptr(unsafe.Pointer(ifreq)))
+	if errno != 0 {
+		return fmt.Errorf("errno=%v", errno)
+	}
+
+	return nil
 }
 
 func CreateTap(macAddress net.HardwareAddr) (string, int, error) {
@@ -39,6 +92,11 @@ func CreateTap(macAddress net.HardwareAddr) (string, int, error) {
 	iface, err := netlink.LinkByName(tapName)
 	if err != nil {
 		return "", -1, fmt.Errorf("failed to lookup %q: %v", tapName, err)
+	}
+
+	err = SetTapSpeed(tapName)
+	if err != nil {
+		return "", -1, fmt.Errorf("failed to set tap speed %q: %v", tapName, err)
 	}
 
 	return tapName, iface.Attrs().Index, nil
